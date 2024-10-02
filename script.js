432d0d73f418526f2b563da61fb7f225b8992080
Tomas <tomas.vicente.7e5@itb.cat>
2024-10-02 22:37:09 +0200

    contact functions added

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function openMenu() {
     const nav = document.querySelector('nav');
     nav.classList.toggle('open');
 }
+
+function Contact() {
+    window.location.href = "https://mail.google.com/mail/?view=cm&fs=1&to=tomas240605@gmail.com";
+}
